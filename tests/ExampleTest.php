941d23c4d6c6d2a555c59f0e5e6c03b000802090
Riina K <riinak.tv@gmail.com>
2022-08-13 15:02:13 +0900

    phpunit, phpmd, phpcs

---
+++ tests/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Remix\UtilityTests;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function testExample(): void
+    {
+        $this->assertTrue(true);
+    }
+}
