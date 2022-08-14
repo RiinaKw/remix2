4aaca77049acf421e226e551e9f7d951d5037599
Riina K <riinak.tv@gmail.com>
2022-08-14 23:54:46 +0900

    DocComment

--- src/Ulitities/PHPUnit/BaseTestCase.php
+++ src/Ulitities/PHPUnit/BaseTestCase.php
@@ -5,6 +5,11 @@ namespace Remix\Utilities\PHPUnit;
 use PHPUnit\Framework\TestCase;
 use Remix\Delay;
 
+/**
+ * Base PHPUnit test case class for Remix
+ *
+ * @package  Remix\Utilities\PHPUnit
+ */
 class BaseTestCase extends TestCase
 {
     protected function setUp(): void
