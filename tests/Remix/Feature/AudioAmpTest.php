2b18415b0840d0de91e8a095a7fb4f0cc63f09bd
Riina K <riinak.tv@gmail.com>
2022-08-16 00:56:13 +0900

    Testing Delay when calling Amp via Audio

--- tests/Remix/Feature/AudioAmpTest.php
+++ tests/Remix/Feature/AudioAmpTest.php
@@ -5,6 +5,8 @@ namespace Remix\Tests;
 use Remix\Utilities\PHPUnit\BaseTestCase;
 use Remix\Audio;
 use Remix\Amp;
+use Remix\Effectors\Noise\Noise;
+use Remix\Delay;
 
 class AudioAmpTest extends BaseTestCase
 {
@@ -15,5 +17,29 @@ class AudioAmpTest extends BaseTestCase
 
         $amp2 = Audio::instance()->amp();
         $this->assertSame($amp, $amp2);
+    }
+
+    public function testDelay(): void
+    {
+        // just to suppress the STDERR
+        $this->expectOutputRegex("//");
+
+        $args = ['noise', '-C', '--voice=Make it louder!!'];
+        Audio::instance()->amp()->play($args);
+        Audio::destroy();
+
+        $expected = [
+            ['type' => 'TRACE', 'log' => '[birth] ' . Audio::class],
+            ['type' => 'TRACE', 'log' => '[birth] ' . Amp::class],
+            ['type' => 'TRACE', 'log' => '[birth] ' . Noise::class],
+            [
+                'type' => 'BODY',
+                'log' => Noise::class . '::index() {"args":{"voice":"Make it louder!!"},"switches":["C"]}'
+            ],
+            ['type' => 'TRACE', 'log' => '[death] ' . Noise::class],
+            ['type' => 'TRACE', 'log' => '[death] ' . Amp::class],
+            ['type' => 'TRACE', 'log' => '[death] ' . Audio::class],
+        ];
+        $this->assertSame($expected, Delay::get());
     }
 }
